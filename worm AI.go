4199f2ae732dd1f3d2f41d2d63caa45939fe8852
Hackersploit__xxSploitation <78088921+hackersploitxxsploitation@users.noreply.github.com>
2023-12-26 18:24:30 -0300

    Add files via upload

---
+++ worm AI.go
@@ -0,0 +1,44 @@
+package main
+import "fmt"
+import "net"
+
+
+
+func AI_geneative_Wordlist(   cvs fle_matriz []byte)([]string){
+
+
+
+
+}//nossa rede neural que cria  wordlist usamos rede neurais no temos que gere nossa word_list
+
+func Fin_Host()([]string){
+
+
+
+
+
+
+}//Enumere Host
+
+
+
+
+func  Tecnique_AI_Lateral([]byte){
+
+
+
+
+
+}//ler nosso aquivo   csv
+func Auto_replique{
+
+
+
+
+
+
+}//Nossa Rede Neural que se utlizara de tecnicas  modernas  de  movimentaçao lateral  
+//empacotamos nosso arquivo  .csv num matruz estatica  de bytes contendo os dados de treinamento nossa rede neural vai ser ensinada
+
+
+func main()
